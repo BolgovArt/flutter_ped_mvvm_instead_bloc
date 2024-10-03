da9df2f67de7fa4b141ce74a64c03969256df434
Артем Болгов <bolgovart@yandex.ru>
2024-10-03 11:37:08 +0300

    переписано на стримы

--- lib/domain/data_providers/data_provider.dart
+++ lib/domain/data_providers/data_provider.dart
@@ -1,21 +1,22 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_ped_mvvm_instead_block/domain/entity/user.dart';
 
-class UserDataProvider extends ChangeNotifier{
-  var _user = User(0);
-  User get user => _user; // в ui/сервисах нужно узнавать о том, что данные эти меняются. Можно вместо ChangeNotifier сделать user в значениии valueNotifier или же сделать здесь стрим, который будет стримить юзеров
-                          // valueNotifier был бы хорош, если в UserDataProvider было бы много всего (зарплаты и тд). Такого в принципе не надо допускать, но если вдруг допустили, то вот.
-                          // стримы позволяют цепочку событий. его недостаток - нельзя посмотреть текущее значение. стрим более мощный чем ChangeNotifier, часть его функционала не нужна, поэтому такое.
+class UserDataProvider{
+  var _user = User(0); 
+  final _controller = StreamController<User>(); // создали стрим
+  Stream<User> get userStream => _controller.stream.asBroadcastStream(); // стрим можно забирать || .asBroadcastStream() - чтобы получать стрим с любого места
+  User get user => _user; // можно получать текущее значение User
+
+
   Timer? _timer;
 
 // Timer - просто суррогатный код
   void openConnect() {
     if (_timer != null) return;
     _timer = Timer.periodic(const Duration(seconds: 1), (_){
-      _user = User(_user.age + 1);
-      notifyListeners();
+      _user = User(_user.age + 1); // в openConnection обновляются как текущие значения тут
+      _controller.add(_user); // так и в стриме
     });
   }
 
--- lib/widgets/example_widget.dart
+++ lib/widgets/example_widget.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ped_mvvm_instead_block/domain/entity/user.dart';
 import 'package:provider/provider.dart';
 
 import 'package:flutter_ped_mvvm_instead_block/domain/services/user_service.dart';
@@ -10,22 +12,28 @@ class ViewModelState {
     required this.ageTitle,
   });
 }
-// ? Почему ViewModel тоже ChangeNotifier?
+
 class ViewModel extends ChangeNotifier{
   final _userService = UserService();
   var _state = ViewModelState(ageTitle: '');
   ViewModelState get state => _state;
+  StreamSubscription<User>? userSubscription;
 
   ViewModel() {
-    _userService.startListenUser((user) {
-      _state = ViewModelState(ageTitle: user.age.toString());
+    _state = ViewModelState(ageTitle: _userService.user.age.toString());
+    userSubscription = _userService.userStream.listen((User user){ // на каждом событии стрима обновляется State и уведомляются подписчики
+      _state = ViewModelState(ageTitle: _userService.user.age.toString());
       notifyListeners();
-    });
+    }); // функция .listen возвращает StreamSubscription, теперь эту подписку можно сохранить, положив прослушку в userSubscription
+    _userService.openConnect();
   }
+
 //? Зачем dispose?
+// подписка начинается когда экран появляется, заканчивать подписку надо когда закрывается экран
   @override
   void dispose() {
-    // _userService.stopListenUser();
+    userSubscription?.cancel();
+    _userService.closeConnect();
     super.dispose();
   }
 }
